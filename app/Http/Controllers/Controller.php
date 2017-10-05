491bff468a6e6dad050f62501b17d294038c2ae6
PD Roux <PaulDermottRoux@gmail.com>
2017-10-05 13:06:09 +0200

    learning shit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test(){
+      $input_data = request()->all();
+      //Validation
+      //AIzaSyDkWWEdNKbbFUV3vH5i_Rv4GPyApMNHyU4
+
+      return $input_data;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/test', 'Controller@test');
