5c1f2112298dd225eb833d001a84cd75c202153d
Sean Roux <searou96@gmail.com>
2017-10-05 14:30:31 +0200

    using guzzle

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,9 +13,14 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function test(){
-      $input_data = request()->all();
-      //Validation
+      $client = new \GuzzleHttp\Client();
+      $res = $client->request('GET', 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=<PUTKEYHERE>');
+      //echo $res->getStatusCode();
+      // 200
+      //echo $res->getHeaderLine('content-type');
+      // 'application/json; charset=utf8'
+      return $res->getBody()->destination_addresses;
+      // '{"id": 1420053, "name": "guzzle", ...}'
 
-      return $input_data;
     }
 }
